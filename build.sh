eacb6d06a52596d608417034bc8d2040b08f5d3d
Donato Wolfisberg <donato@wolfisberg.dev>
2021-03-12 16:27:15 +0000

    faile build when there is no index.html in public dir

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
+if [ ! -f "public/index.html" ]
+then
+    echo "Could not find index.html in public directory"
+    exit 1
+fi
+
 CGO_ENABLED=0 GOOS=linux go build -mod=readonly -v -o server
